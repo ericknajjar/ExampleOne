ecaf573c1e18e2a7e6198f4c849828778531d05d
Erick <enajjar@gazeus.com.br>
2019-08-06 15:08:39 -0300

    first

---
+++ Assets/Example.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    private void Update()
+    {
+
+        var list = new List<int>();
+        var toAddNext = new List<int>();
+
+        for (int i = 0; i < 1000; ++i)
+        {
+            int val = UnityEngine.Random.Range(0, 20000);
+            int valNext = UnityEngine.Random.Range(0, 20000);
+            toAddNext.Add(valNext);
+            list.Add(val);
+
+        }
+
+        int max = ReturnMax(list, toAddNext);
+
+        Debug.LogFormat("Max {0} ", max);
+    }
+
+
+    public int ReturnMax(List<int> list, List<int> next)
+    {
+        // 1 - Adiciona o novo elemento a coleção
+        // 2 - Remove o Maior element da coleção
+        // 3 - Retorna o maior elemento presente na coleção ao final
+        for (int i = 0; i < next.Count; ++i)
+        {
+            var toAdd = next[i];
+            list.Add(toAdd);
+            list.Sort();
+            list.RemoveAt(list.Count - 1);
+        }
+
+        return list[list.Count - 1];
+    }
+}
